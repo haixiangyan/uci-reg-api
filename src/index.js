09d1ebe8f538ce06cc1ba12adcdd7c6eb66010ae
Haixiang6123 <haixiang6123@gmail.com>
2019-07-03 23:37:12 -0700

    Add RegParser

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import 'babel-polyfill'
 import axios from 'axios'
 import cheerio from 'cheerio'
 import {requestOptions} from './http/request'
+import RegParser from './parser/RegParser'
 
 axios(requestOptions)
     .then(response => {
         const dom = cheerio.load(response.data)
-        console.log(dom.html())
+        const regParser = new RegParser(dom)
+
+        regParser.parse()
     })
