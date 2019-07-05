8220922a948b354d6e5a773dddc866a1533f6294
Haixiang6123 <haixiang6123@gmail.com>
2019-07-05 08:44:11 -0700

    Add validation

--- src/parser/RegParser.ts
+++ src/parser/RegParser.ts
@@ -1,25 +1,70 @@
-interface ICourse {
+import { validate, courseDetailsAttr, blueBarAttr } from '../utils/validation'
+interface Course {
     title: string,
+    details: CourseDetails
 }
 
-export default class RegParser {
-    private $: any
+interface CourseDetails {
+    Code?: string,
+    Type?: string,
+    Sec?: string,
+    Units?: string,
+    Instructor?: string,
+    Time?: string,
+    Place?: string,
+    Final?: string,
+    Max?: string,
+    Enr?: string,
+    WL?: string,
+    Req?: string,
+    Nor?: string,
+    Rstr?: string,
+    Textbooks?: string,
+    Web?: string,
+    Status?: string
+}
+
+class RegParser {
+    private $: CheerioStatic
+
     constructor(dom: CheerioStatic) {
         this.$ = dom
     }
 
-    parse() {
-        let courses: ICourse[] = []
-        this.$('.CourseTitle').each((index: number, element: CheerioElement) => {
-            let course: ICourse = {
-                title: undefined
+    parse(): Course[] {
+        let courses: Course[] = []
+        this.$('.CourseTitle').each((index: number, courseTitleElement: CheerioElement) => {
+            let course: Course = {
+                title: undefined,
+                details: {}
             }
 
-            course.title = this.$(element).text().trim().split(/\s{2,}/)
+            // Get course title
+            course.title = this.$(courseTitleElement).text().trim().split(/\s{2,}/).join(' ')
 
+            // Get course info
+            course.details = this.parseCourseDetails(courseTitleElement)
+
+            // Add course object to list
             courses.push(course)
         })
 
-        console.log(courses)
+        return courses
+    }
+
+    parseCourseDetails(courseTitleElement: CheerioElement): CourseDetails {
+        let courseDetails = {}
+        let currentElement = courseTitleElement
+
+        while (!validate(currentElement, blueBarAttr)) {
+            if (validate(currentElement, courseTitleElement)) {
+                console.log(currentElement)
+            }
+            currentElement = currentElement.nextSibling
+        }
+
+        return courseDetails
     }
 }
+
+export default RegParser
---
+++ src/utils/validation.ts
@@ -0,0 +1,16 @@
+export const courseDetailsAttr = {
+    valign: 'top',
+}
+export const blueBarAttr = {
+    class: 'blue-bar',
+    bgcolor: 'navy'
+}
+
+export const validate = (element: CheerioElement, attr: object): boolean => {
+    for (let [key, value] of Object.entries(attr)) {
+        if (element.attribs[key] !== value) {
+            return false
+        }
+    }
+    return true
+}
