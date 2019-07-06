77a588b775417146ca8ceb1e037bb232c0998649
Haixiang6123 <haixiang6123@gmail.com>
2019-07-05 22:06:50 -0700

    Add services folder

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,6 @@
 import 'babel-polyfill'
-import axios from 'axios'
-import cheerio from 'cheerio'
-import {requestOptions} from './http/request'
-import CourseParser from './parser/CourseParser'
+import {getCoursePage} from "./http/courseService"
 
-axios(requestOptions)
-    .then(response => {
-        const dom: CheerioStatic = cheerio.load(response.data)
-        const regParser = new CourseParser(dom)
-
-        const courses = regParser.parse()
-    })
+getCoursePage().then((courses) => {
+    console.log(courses)
+})
