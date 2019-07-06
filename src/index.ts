0ccbb180484d65c164039147b217812fd791f09c
Haixiang6123 <haixiang6123@gmail.com>
2019-07-06 11:15:39 -0700

    Add default request body

--- src/index.ts
+++ src/index.ts
@@ -3,23 +3,52 @@ import cheerio from 'cheerio'
 import fs from 'fs'
 import {getOptionPage} from './services/optionService'
 import OptionParser from "./parser/OptionParser"
-// getCoursePage().then((data) => {
-//     const dom: CheerioStatic = cheerio.load(data)
-//
-//     const regParser = new CourseParser(dom)
-//
-//     const courses = regParser.parse()
-//     console.log(courses)
-// })
+import {getCoursePage} from "./services/courseService"
+import CourseParser from "./parser/CourseParser"
+import {RequestBody} from "./types"
 
-getOptionPage().then(response => {
-    const dom: CheerioStatic = cheerio.load(response.data)
+const defaultRequestBody: RequestBody = {
+    'YearTerm': '2019-92',
+    'ShowComments': 'on',
+    'ShowFinals': 'on',
+    'Breadth': 'ANY',
+    'Dept': 'NET SYS',
+    'CourseNum': '',
+    'Division': 'ANY',
+    'CourseCodes': '',
+    'InstrName': '',
+    'CourseTitle': '',
+    'ClassType': 'ALL',
+    'Units': '',
+    'Days': '',
+    'StartTime': '',
+    'EndTime': '',
+    'MaxCap': '',
+    'FullCourses': 'ANY',
+    'FontSize': '100',
+    'CancelledCourses': 'Exclude',
+    'Bldg': '',
+    'Room': '',
+    'Submit': 'Display Web Results'
+}
 
-    const selectionParser = new OptionParser(dom)
+getCoursePage(defaultRequestBody).then((data) => {
+    const dom: CheerioStatic = cheerio.load(data)
 
-    const options = selectionParser.parse()
-    console.log(options)
-    fs.writeFile('output.js', options, () => {
-        console.log('Success')
-    })
+    const regParser = new CourseParser(dom)
+
+    const courses = regParser.parse()
+    console.log(courses)
 })
+
+// getOptionPage().then(response => {
+//     const dom: CheerioStatic = cheerio.load(response.data)
+//
+//     const selectionParser = new OptionParser(dom)
+//
+//     const options = selectionParser.parse()
+//     console.log(options)
+//     fs.writeFile('output.js', options, () => {
+//         console.log('Success')
+//     })
+// })
--- src/services/courseService.ts
+++ src/services/courseService.ts
@@ -1,34 +1,10 @@
 import axios, {AxiosRequestConfig} from 'axios'
 import qs from 'qs'
 import {baseUrl} from "../config"
-
-const requestBody = {
-    'YearTerm': '2019-92',
-    'ShowComments': 'on',
-    'ShowFinals': 'on',
-    'Breadth': 'ANY',
-    'Dept': 'EECS',
-    'CourseNum': '',
-    'Division': 'ANY',
-    'CourseCodes': '',
-    'InstrName': '',
-    'CourseTitle': '',
-    'ClassType': 'ALL',
-    'Units': '',
-    'Days': '',
-    'StartTime': '',
-    'EndTime': '',
-    'MaxCap': '',
-    'FullCourses': 'ANY',
-    'FontSize': 100,
-    'CancelledCourses': 'Exclude',
-    'Bldg': '',
-    'Room': '',
-    'Submit': 'Display Web Results'
-}
+import {RequestBody} from "../types"
 
 // Method to get course info
-export const getCoursePage = async () => {
+export const getCoursePage = async (requestBody: RequestBody) => {
     const options: AxiosRequestConfig = {
         method: 'POST',
         headers: { 'content-type': 'application/x-www-form-urlencoded' },
