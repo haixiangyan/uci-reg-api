3e557758966ea648b54b05fa4ab6464d301d9fb3
Haixiang6123 <haixiang6123@gmail.com>
2019-07-06 13:32:28 -0700

    Update entry point file

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,18 @@ import 'babel-polyfill'
 import OptionParser from "./parser/OptionParser"
 import CourseParser from "./parser/CourseParser"
 
+// Usage
+// CourseParser.getInstance({}).then(parser => {
+//     const courses = parser.parse()
+//     console.log(courses)
+// })
+//
+// OptionParser.getInstance().then(parser => {
+//     const options = parser.parse()
+//     console.log(options)
+// })
 
-CourseParser.getInstance({}).then(parser => {
-    const courses = parser.parse()
-    console.log(courses)
-})
-
-OptionParser.getInstance().then(parser => {
-    const options = parser.parse()
-    console.log(options)
-})
+export default {
+    OptionParser,
+    CourseParser
+}
