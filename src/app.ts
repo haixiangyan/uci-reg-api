d829f3d77b3a4d23fbf6888bbbe1bab10a440c22
Haixiang6123 <haixiang6123@gmail.com>
2019-07-06 21:35:28 -0700

    Update 404 error

--- src/app.ts
+++ src/app.ts
@@ -1,33 +1,23 @@
-import createError from 'http-errors'
-import express, {NextFunction, Request, Response} from 'express'
+import express from 'express'
 import logger from 'morgan'
 
 import optionsRouter from './routes/options'
 import coursesRouter from './routes/courses'
 
-const app = express();
+const app = express()
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// Use middle wares
+app.use(express.static(__dirname + '/public'));
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
-app.use('/options', optionsRouter);
-app.use('/courses', coursesRouter);
+app.use('/options', optionsRouter)
+app.use('/courses', coursesRouter)
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err: any, req: Request, res: Response, next: NextFunction) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.use(function (req, res, next) {
+    res.status(404).send('Not Found, try /options or /courses')
+})
 
 export default app
